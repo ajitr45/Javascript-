d031d48d637429e579eabca70b92005001b0eda8
Ajit kumar <ajit62768@gmail.com>
2025-09-16 08:15:40 +0000

    variable discussion

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,27 @@
+const accountId = 25468
+let accountEmail = "ajit125@gamil.com"
+var accountPassword = "124586"
+accountCity = "Delhi"
+
+
+// accountId = 125468
+
+// console.log(accountId); not allowed
+
+accountEmail = "hyadav1254@gmail.com"
+accountPassword = "465834"
+accountCity = "Assam"
+let accountState;
+
+console.log(accountId);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
