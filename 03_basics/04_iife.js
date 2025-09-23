e3bbc7fac2b619d48a08eb00ca0c5cc62a8412cd
Ajit kumar <ajit62768@gmail.com>
2025-09-23 17:10:27 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// Imediately invoked function expression (IIFE)
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+}) ("hitesh")
